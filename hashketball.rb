239e6ed2a123425ebd2606d8d711a51d2faa9df3
Huy Do <huyddo40@gmail.com>
2019-06-02 21:00:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -241,8 +241,7 @@ def player_stats(name)
           if attribute == :players
               data.each do |player, stat|
                 if player == name
-                  # stats = stat
-                  stats = game_hash[location][player]
+                  stats = game_hash[location][:players]
                   binding.pry
                 end
              end
